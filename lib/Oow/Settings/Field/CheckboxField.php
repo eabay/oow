5e21dfcc77d653c9e3f60b80393327274a6a9273
Erhan Abay <erhanabay@gmail.com>
2013-01-15 17:59:36 +0200

    unnecessary for attribute in label tag

--- lib/Oow/Settings/Field/CheckboxField.php
+++ lib/Oow/Settings/Field/CheckboxField.php
@@ -14,7 +14,7 @@ class CheckboxField extends AbstractField
             $this->attr['checked'] = 'checked';
         }
 
-        $html = sprintf('<label for="%1$s_%2$s"><input id="%1$s_%2$s" name="%1$s[%2$s]" %3$s> <span class="description">%4$s</span></label>',
+        $html = sprintf('<label><input id="%1$s_%2$s" name="%1$s[%2$s]" %3$s> <span class="description">%4$s</span></label>',
             $this->optionName,
             $this->id,
             $this->getAttribs(),
