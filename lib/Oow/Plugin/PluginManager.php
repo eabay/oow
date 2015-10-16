ce7239473b1a07ce0b85d79073acf680fe81fbf8
Erhan Abay <erhanabay@gmail.com>
2015-10-17 00:06:06 +0300

    Use `wp_send_json()` for JSON and use `wp_die()` for text response in `AjaxResponse` annotations

--- lib/Oow/Plugin/PluginManager.php
+++ lib/Oow/Plugin/PluginManager.php
@@ -9,20 +9,20 @@ use Doctrine\Common\Annotations\SimpleAnnotationReader;
 class PluginManager
 {
     protected $annotationReader;
-    
+
     public function __construct(Reader $reader = null)
     {
         AnnotationRegistry::registerFile(__DIR__ . '/Annotations/PluginAnnotations.php');
-        
+
         if ($reader) {
             $this->setAnnotationReader($reader);
         }
     }
-    
+
     public function setAnnotationReader(Reader $reader)
     {
         $this->annotationReader = $reader;
-        
+
         return $this;
     }
 
@@ -34,21 +34,21 @@ class PluginManager
         if (!$this->annotationReader) {
             $reader = new SimpleAnnotationReader;
             $reader->addNamespace('Oow\Plugin\Annotations');
-            
+
             $this->annotationReader = $reader;
         }
-        
+
         return $this->annotationReader;
     }
-    
+
     public function addPlugin($plugin)
     {
         $reflClass = new \ReflectionClass($plugin);
-        
+
         if (!$this->getAnnotationReader()->getClassAnnotation($reflClass, 'Oow\Plugin\Annotations\Plugin')) {
             throw new \InvalidArgumentException("{$reflClass->getName()} does not have any Oow\\Plugin\\Annotations\\Plugin annotation instance");
         }
-        
+
         foreach ($reflClass->getMethods() as $method) {
             if ($method->isPublic()) {
                 foreach ($this->getAnnotationReader()->getMethodAnnotations($method) as $annot) {
@@ -64,7 +64,7 @@ class PluginManager
                     } elseif ($annot instanceof Annotations\Shortcode) {
                         $tag  = $annot->tag;
                         $func = array($plugin, $method->getName());
-                        
+
                         add_shortcode($tag, $func);
                     } elseif ($annot instanceof Annotations\AjaxResponse) {
                         $closure = function() use ($plugin, $method, $annot) {
@@ -75,13 +75,11 @@ class PluginManager
                             }
 
                             if ($annot->json) {
-                                header('Content-Type: application/json');
-                                echo json_encode($response);
+                                wp_send_json($response);
                             } else {
                                 echo $response;
+                                wp_die();
                             }
-
-                            exit;
                         };
 
                         add_action('wp_ajax_'. $annot->action, $closure);
@@ -100,36 +98,36 @@ class PluginManager
                 }
             }
         }
-        
+
         return $this;
     }
-    
+
     public function addPlugins(array $plugins)
     {
         foreach ($plugins as $plugin) {
             $this->addPlugin($plugin);
         }
     }
-    
+
     public function addWidget($widget)
     {
         $reflClass = new \ReflectionClass($widget);
-        
+
         if (!$this->getAnnotationReader()->getClassAnnotation($reflClass, 'Oow\Plugin\Annotations\Widget')) {
             throw new \InvalidArgumentException("{$reflClass->getName()} does not have any Oow\\Plugin\\Annotations\\Widget annotation instance");
         }
-        
+
         add_action('widgets_init', function () use ($widget) {
             global $wp_widget_factory;
-            
+
             $wp_widget_factory->widgets[get_class($widget)] = $widget;
         });
-        
+
         $this->addPlugin($widget);
-        
+
         return $this;
     }
-    
+
     public function addWidgets(array $widgets)
     {
         foreach ($widgets as $widget) {
